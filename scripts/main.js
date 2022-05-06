58dd6fa79641ce43f19d34563fae2dfe11efc9b5
faustocalvinio <faustocalvino@outlook.com>
2022-05-06 18:23:35 +0200

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,50 @@
-
-let qs=(param)=>document.querySelector(param);
+const qs=(param)=>document.querySelector(param);
 const qsa=(param)=>document.querySelectorAll(param);
 
 const daily=qs('#daily');
 const weekly=qs('#weekly');
 const monthly=qs('#monthly');
-
-const getData=async()=>{
-    try{
-    await fetch('../data.json')
-    .then(response=>{    
-   
-    return response.json();  
-    })
-    .then(data=>console.log(data[0].timeframes.daily.current))
-    }catch(err){
-    console.err(err)
-}};    
-
-// error de tiempo, al hacer click no existe la data
-getData();
-
+const subjectsArray=qsa('.subject');
 const arrayHoursText=document.querySelectorAll('.text-hrs')
+const lastHoursArr=document.querySelectorAll('.l-w-hours-span')
+const url='../data.json'
+
+fetch(url)
+.then(response=>response.json())
+.then(data=>finalData=data)
+
+
+
 daily.addEventListener('click',()=>{
-   arrayHoursText[0].innerHTML=data[0].timeframes.daily.current;
-   arrayHoursText[1].innerHTML='asdasd'
+    monthly.classList.toggle('active',false)
+    weekly.classList.toggle('active',false)
+    daily.classList.toggle('active')
+    for (let index = 0; index <=5; index++) {
+        subjectsArray[index].innerHTML=finalData[index].title;
+        arrayHoursText[index].innerHTML=finalData[index].timeframes.daily.current;
+        lastHoursArr[index].innerHTML=finalData[index].timeframes.daily.previous;        
+    }
 })
 
 weekly.addEventListener('click',()=>{
-    alert('b')
+    daily.classList.toggle('active',false)
+    monthly.classList.toggle('active',false)
+    weekly.classList.toggle('active')
+    for (let index = 0; index <=5; index++) {
+        subjectsArray[index].innerHTML=finalData[index].title;
+        arrayHoursText[index].innerHTML=finalData[index].timeframes.weekly.current;
+        lastHoursArr[index].innerHTML=finalData[index].timeframes.weekly.previous;                
+    }
 });
 
 monthly.addEventListener('click',()=>{
-    alert('bc')
+    daily.classList.toggle('active',false)
+    weekly.classList.toggle('active',false)
+    monthly.classList.toggle('active')
+    for (let index = 0; index <=5; index++) {
+        subjectsArray[index].innerHTML=finalData[index].title;
+        arrayHoursText[index].innerHTML=finalData[index].timeframes.monthly.current;        
+        lastHoursArr[index].innerHTML=finalData[index].timeframes.monthly.previous;        
+    }
 })
+
